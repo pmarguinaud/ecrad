ef041c8e5e014e931f5e0c1180f23f78592ffe4d
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-03-22 09:05:09 +0000

    ÃAdd compile_gpu.sh

---
+++ compile_gpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+ml prgenv/nvidia nvidia/22.11 netcdf4/4.9.1 nco/4.9.7 fcm/2021.05.0 openblas/0.3.21
+
+set -x
+
+make PROFILE=pgi BITIDENTITY_TESTING=1 BLASLIB=openblas GPU=1 -j32 ifsdriver
+
